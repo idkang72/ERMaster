f0c900a15d0617b0ae0cd08752d8ba487f41c366
idkang72 <idkang72@gmail.com>
2026-09-22 10:27:05 +0900

    Fix: 미지정 앵커(xp/yp == -1) 대상 연결선에 직각 진입(Orthogonal alignment) 보정 로직 구현

--- ermaster_plugin/src/main/java/ermaster/editor/view/figure/anchor/XYChopboxAnchor.java
+++ ermaster_plugin/src/main/java/ermaster/editor/view/figure/anchor/XYChopboxAnchor.java
@@ -1,8 +1,10 @@
 package ermaster.editor.view.figure.anchor;
 
+
 import org.eclipse.draw2d.ChopboxAnchor;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.geometry.Point;
+import org.eclipse.draw2d.geometry.Rectangle;
 
 public class XYChopboxAnchor extends ChopboxAnchor {
 
@@ -12,10 +14,12 @@ public class XYChopboxAnchor extends ChopboxAnchor {
 		super(owner);
 	}
 
+
 	public void setLocation(Point location) {
 		this.location = location;
 		this.fireAnchorMoved();
 	}
+
 
 	/**
 	 * {@inheritDoc}
@@ -25,11 +29,52 @@ public class XYChopboxAnchor extends ChopboxAnchor {
 		if (this.location != null) {
 			Point point = new Point(this.location);
 			this.getOwner().translateToAbsolute(point);
+
+
 			return point;
 		}
 
+		if (reference != null) {
+			Rectangle r = new Rectangle(this.getBox());
+			r.translate(-1, -1);
+			r.resize(1, 1);
+			this.getOwner().translateToAbsolute(r);
+
+			if (! r.isEmpty() && ! r.contains(reference)) {
+				if (reference.y >= r.y && reference.y <= r.bottom()) {
+					if (reference.x < r.x) {
+
+
+						return new Point(r.x, reference.y);
+					}
+
+					if (reference.x > r.right()) {
+
+
+						return new Point(r.right(), reference.y);
+					}
+				}
+
+				if (reference.x >= r.x && reference.x <= r.right()) {
+					if (reference.y < r.y) {
+
+
+						return new Point(reference.x, r.y);
+					}
+
+					if (reference.y > r.bottom()) {
+
+
+						return new Point(reference.x, r.bottom());
+					}
+				}
+			}
+		}
+
+
 		return super.getLocation(reference);
 	}
+
 
 	/**
 	 * {@inheritDoc}
@@ -39,8 +84,11 @@ public class XYChopboxAnchor extends ChopboxAnchor {
 		if (this.location != null) {
 			Point point = new Point(this.location);
 			this.getOwner().translateToAbsolute(point);
+
+
 			return point;
 		}
+
 
 		return super.getReferencePoint();
 	}
